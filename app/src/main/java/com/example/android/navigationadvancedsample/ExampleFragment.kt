99db909a429046e6a0bdf32357514d2b7ff13faa
Android <android@syneforge.com>
2023-09-29 12:01:02 +0300

    first

---
+++ app/src/main/java/com/example/android/navigationadvancedsample/ExampleFragment.kt
@@ -0,0 +1,30 @@
+package com.example.android.navigationadvancedsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+
+class ExampleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_example, container, false)
+
+        val userName = arguments?.getString("userName")
+
+        view.findViewById<TextView>(R.id.tvName).text = userName
+        view.findViewById<Button>(R.id.btnProceed).setOnClickListener {
+            findNavController().navigate(R.id.action_exampleFragment_to_userProfile, arguments)
+        }
+
+        return view
+    }
+}
